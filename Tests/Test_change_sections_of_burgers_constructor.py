9040f67a305a9d7d6108b354d003c18f0bf8452f
Vladislav Semenov <stori.semen@yandex.ru>
2024-12-15 13:41:07 +0300

    Добавлено описание локаторов в файле locators.py. Скорректированы названия локаторов HEAD_NONACTIVE_FIRST и HEAD_NONACTIVE_SECOND. Скорректировано название файла Test_Move_Between_Different_Sections_Of_App.py.

--- Tests/Test_change_sections_of_burgers_constructor.py
+++ Tests/Test_change_sections_of_burgers_constructor.py
@@ -15,7 +15,7 @@ def test_successful_switch_to_section_sauces_in_constructor(driver):
 # Проверяем успешное переключение на раздел конструктора "Соусы".
     WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(ConstructorSection.BULKI_SECTION_ACTIVE))
 
-    driver.find_element(*ConstructorSection.HEAD_NONACTIVE_SAUCES).click()
+    driver.find_element(*ConstructorSection.HEAD_NONACTIVE_FIRST).click()
 
     section_sauces = driver.find_element(*ConstructorSection.HEAD_SECTIONS_SAUCES_FILLINGS_ACTIVE)
 
@@ -25,7 +25,7 @@ def test_successful_switch_to_section_fillings_in_constructor(driver):
 # Проверяем успешное переключение на раздел конструктора "Начинки".
     WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(ConstructorSection.BULKI_SECTION_ACTIVE))
 
-    driver.find_element(*ConstructorSection.HEAD_NONACTIVE_FILLINGS).click()
+    driver.find_element(*ConstructorSection.HEAD_NONACTIVE_SECOND).click()
 
     section_fillings = driver.find_element(*ConstructorSection.HEAD_SECTIONS_SAUCES_FILLINGS_ACTIVE)
 
