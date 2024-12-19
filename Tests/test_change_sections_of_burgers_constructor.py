7f7fc8eed1cf7c7032185e475b682fd1da1d58d1
Vladislav Semenov <stori.semen@yandex.ru>
2024-12-19 23:53:27 +0300

    Ошибочный merge.

--- Tests/test_change_sections_of_burgers_constructor.py
+++ Tests/test_change_sections_of_burgers_constructor.py
@@ -3,32 +3,30 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from locators import ConstructorSection
 
-class TestSectionBurgerConstructor:
-
-    def test_section_bulki_in_constructor_on_main_page_app(self, driver):
+def test_section_bulki_in_constructor_on_main_page_app(driver):
     #Проверяем, что при первоначальной загрузке главной страницы приложения с конструктором выбран раздел "Булки".
-        WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(ConstructorSection.BULKI_SECTION_ACTIVE))
+    WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(ConstructorSection.BULKI_SECTION_ACTIVE))
 
-        section_bulki = driver.find_element(*ConstructorSection.HEAD_ACTIVE_SECTION)
+    section_bulki = driver.find_element(*ConstructorSection.HEAD_ACTIVE_SECTION)
 
-        assert section_bulki.text == 'Булки'
+    assert section_bulki.text == 'Булки'
 
-    def test_successful_switch_to_section_sauces_in_constructor(self, driver):
+def test_successful_switch_to_section_sauces_in_constructor(driver):
 # Проверяем успешное переключение на раздел конструктора "Соусы".
-        WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(ConstructorSection.BULKI_SECTION_ACTIVE))
+    WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(ConstructorSection.BULKI_SECTION_ACTIVE))
 
-        driver.find_element(*ConstructorSection.HEAD_NONACTIVE_FIRST).click()
+    driver.find_element(*ConstructorSection.HEAD_NONACTIVE_FIRST).click()
 
-        section_sauces = driver.find_element(*ConstructorSection.HEAD_SECTIONS_SAUCES_FILLINGS_ACTIVE)
+    section_sauces = driver.find_element(*ConstructorSection.HEAD_SECTIONS_SAUCES_FILLINGS_ACTIVE)
 
-        assert section_sauces.text == 'Соусы'
+    assert section_sauces.text == 'Соусы'
 
-    def test_successful_switch_to_section_fillings_in_constructor(self, driver):
+def test_successful_switch_to_section_fillings_in_constructor(driver):
 # Проверяем успешное переключение на раздел конструктора "Начинки".
-        WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(ConstructorSection.BULKI_SECTION_ACTIVE))
+    WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(ConstructorSection.BULKI_SECTION_ACTIVE))
 
-        driver.find_element(*ConstructorSection.HEAD_NONACTIVE_SECOND).click()
+    driver.find_element(*ConstructorSection.HEAD_NONACTIVE_SECOND).click()
 
-        section_fillings = driver.find_element(*ConstructorSection.HEAD_SECTIONS_SAUCES_FILLINGS_ACTIVE)
+    section_fillings = driver.find_element(*ConstructorSection.HEAD_SECTIONS_SAUCES_FILLINGS_ACTIVE)
 
-        assert section_fillings.text == 'Начинки'
+    assert section_fillings.text == 'Начинки'
--- Tests/test_registration_system.py
+++ Tests/test_registration_system.py
@@ -1,49 +1,49 @@
 #Проверка успешной регистрации в приложении при вводе валидных значений логина и пароля.
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-from helper import generate_random_email, generate_random_password # Импортируем функции генерации email и пароля из helper.py
+from generators import generate_random_email, generate_random_password # Импортируем функции генерации email и пароля из generators.py
 from locators import HeaderLocators,LoginPageLocators, RegistrationPageLocators
-from data import Credentials
-from curl import *
 
-class TestRegistrationWithValidData:
-
-    def test_successful_registration(self, driver):
+def test_registration_with_valid_data(driver):
 #Нажимаем кнопку Личный кабинет в шапке приложения.
-        driver.find_element(*HeaderLocators.ACCOUNT_BUTTON).click()
-        WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(LoginPageLocators.REGISTER_BUTTON))
+    driver.find_element(*HeaderLocators.ACCOUNT_BUTTON).click()
+
+    WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(LoginPageLocators.REGISTER_BUTTON))
 
 #Нажимаем кнопку "Зарегистрироваться" в футере.
-        driver.find_element(*LoginPageLocators.REGISTER_BUTTON).click()
+    driver.find_element(*LoginPageLocators.REGISTER_BUTTON).click()
 
 # Генерируем случайные данные для регистрации
-        random_email = generate_random_email()  # Генерация случайного email
-        random_password = generate_random_password()  # Генерация случайного пароля
+    random_email = generate_random_email()  # Генерация случайного email
+    random_password = generate_random_password()  # Генерация случайного пароля
 
-#Заполняем поля Имя, Email и Пароль валидными данными для регистрации в приложении, нажимаем кнопку.
-        driver.find_element(*RegistrationPageLocators.NAME_FIELD).send_keys(Credentials.name)
-        driver.find_element(*RegistrationPageLocators.EMAIL_FIELD).send_keys(random_email)
-        driver.find_element(*RegistrationPageLocators.PASSWORD_FIELD).send_keys(random_password)
-        driver.find_element(*RegistrationPageLocators.SUBMIT_BUTTON).click()
+#Заполняем поля Имя, Email и Пароль валидными данными для регистрации в приложении, нажимаем кнопку .
+    driver.find_element(*RegistrationPageLocators.NAME_FIELD).send_keys("Владислав")
+    driver.find_element(*RegistrationPageLocators.EMAIL_FIELD).send_keys(random_email)
+    driver.find_element(*RegistrationPageLocators.PASSWORD_FIELD).send_keys(random_password)
+    driver.find_element(*RegistrationPageLocators.SUBMIT_BUTTON).click()
 
-        WebDriverWait(driver, 10).until(expected_conditions.url_to_be(login_url))
-        assert driver.current_url == login_url
+    WebDriverWait(driver, 10).until(expected_conditions.url_to_be("https://stellarburgers.nomoreparties.site/login"))
+    assert driver.current_url == "https://stellarburgers.nomoreparties.site/login"
 
 
-class TestRegistrationWithInvalidPasswordError:
+def test_registration_with_invalid_password_error(driver):
 
-    def test_invalid_password_error(self, driver):
 #Нажимаем кнопку Личный кабинет в шапке приложения.
-        driver.find_element(*HeaderLocators.ACCOUNT_BUTTON).click()
-        WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(LoginPageLocators.REGISTER_BUTTON))
+    driver.find_element(*HeaderLocators.ACCOUNT_BUTTON).click()
+
+    WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(LoginPageLocators.REGISTER_BUTTON))
+
 #Нажимаем кнопку "Зарегистрироваться" в футере.
-        driver.find_element(*LoginPageLocators.REGISTER_BUTTON).click()
+    driver.find_element(*LoginPageLocators.REGISTER_BUTTON).click()
+
 # Заполняем поле Пароль невалидным значением.
-        driver.find_element(*RegistrationPageLocators.PASSWORD_FIELD).send_keys(Credentials.incorrect_password)
+    driver.find_element(*RegistrationPageLocators.PASSWORD_FIELD).send_keys("qwer1")
 #Снимаем фокус с активного поля Пароль.
-        driver.execute_script("document.activeElement.blur();")
+    driver.execute_script("document.activeElement.blur();")
+
 #Проверяем появление ошибки с текстом
-        error_mes = WebDriverWait(driver, 10).until(
-            expected_conditions.visibility_of_element_located(RegistrationPageLocators.PASSWORD_ERROR_TEXT)
-        )
-        assert error_mes.text == 'Некорректный пароль'
+    error_mes = WebDriverWait(driver, 10).until(
+        expected_conditions.visibility_of_element_located(RegistrationPageLocators.PASSWORD_ERROR_TEXT)
+    )
+    assert error_mes.text == 'Некорректный пароль'
